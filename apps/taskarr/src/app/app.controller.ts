177ae7ff3cb6e26c1538b3446f75834401208fb5
Ang Puo Syer <ang@tawk.to>
2024-10-03 13:25:05 +0800

    feat: 🎸 connect application to db
    register kysely module to nestjs

--- apps/taskarr/src/app/app.controller.ts
+++ apps/taskarr/src/app/app.controller.ts
@@ -1,13 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 
-import { AppService } from './app.service';
+import { AppService } from './app.service'
+import { InjectKysely } from 'nestjs-kysely'
+import { Kysely } from 'kysely';
+import { Db } from '@taskarr/db'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService, @InjectKysely() private readonly db: Kysely<Db>) { }
 
-  @Get()
-  getData() {
-    return this.appService.getData();
-  }
+    @Get()
+    getData() {
+        return this.appService.getData();
+    }
+
+    @Get('/tasks')
+    async getTasks() {
+        return this.db.selectFrom('task').selectAll().execute()
+    }
 }
--- apps/taskarr/src/app/app.module.ts
+++ apps/taskarr/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KyselyModule } from 'nestjs-kysely'
+import { PostgresDialect } from 'kysely';
+import { Pool } from 'pg'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        KyselyModule.forRoot({
+            dialect: new PostgresDialect({
+                pool: new Pool({
+                    host: 'localhost',
+                    port: 5432,
+                    user: 'postgres',
+                    password: 'your-super-secret-and-long-postgres-password',
+                    database: 'postgres',
+                }),
+            }),
+        })],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
